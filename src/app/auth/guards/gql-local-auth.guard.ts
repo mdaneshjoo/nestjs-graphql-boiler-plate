2b0990a10ed3713e20ea437395b23302d94bfbaf
MDaneshjoo <m_daneshjoo@felixin.io>
2023-06-17 20:55:36 +0330

    feat(auth):remove passport

--- src/app/auth/guards/gql-local-auth.guard.ts
+++ src/app/auth/guards/gql-local-auth.guard.ts
@@ -1,14 +1,47 @@
-import { ExecutionContext, Injectable } from '@nestjs/common';
+import {
+  BadRequestException,
+  ExecutionContext,
+  Injectable,
+  UnauthorizedException,
+} from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
 import { GqlExecutionContext } from '@nestjs/graphql';
+import { validate } from 'class-validator';
+import LoginInput from '../dto/login.Input';
+import AuthService from '../auth.service';
 
 @Injectable()
 export default class LocalAuthGuard extends AuthGuard('local') {
+  constructor(private readonly authService: AuthService) {
+    super();
+  }
+
   getRequest(context: ExecutionContext) {
     const ctx = GqlExecutionContext.create(context);
     const request = ctx.getContext().req;
     const { loginInput } = ctx.getArgs();
     request.body = loginInput;
     return request;
+  }
+
+  async canActivate(context: ExecutionContext): Promise<boolean> {
+    const ctx = GqlExecutionContext.create(context);
+
+    const loginInput = new LoginInput();
+    Object.assign(loginInput, ctx.getArgs().loginInput);
+
+    const errors = await validate(loginInput, { stopAtFirstError: true });
+    if (errors.length) {
+      const firstError =
+        errors[0].constraints[Object.keys(errors[0].constraints)[0]];
+      throw new BadRequestException(firstError);
+    }
+    const user = await this.authService.validate(
+      loginInput.email,
+      loginInput.password,
+    );
+    if (!user) throw new UnauthorizedException('you cant login');
+    this.getRequest(context).user = user;
+    return true;
   }
 }
